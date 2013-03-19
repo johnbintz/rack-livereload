6b93954231663be8ed3d1262b8458606ccdffca0
Peter Fern <github@obfusc8.org>
2013-03-19 17:32:46 +1100

    While we're at not messing with stuff, don't mess with inlined content

--- lib/rack/livereload.rb
+++ lib/rack/livereload.rb
@@ -59,7 +59,9 @@ module Rack
       else
         status, headers, body = result = @app.call(env)
 
-        if (headers['Content-Type'] and headers['Content-Type']['text/event-stream']) or headers['Transfer-Encoding'] == 'chunked'
+        if (headers['Content-Type'] and headers['Content-Type']['text/event-stream']) or
+            headers['Transfer-Encoding'] == 'chunked' or
+            headers['Content-Disposition'] =~ %r{^inline}
           return result 
         end
 
